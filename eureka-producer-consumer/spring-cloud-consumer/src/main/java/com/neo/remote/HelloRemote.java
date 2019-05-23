7f063508d004b416f50dde26c734f4b266743591
jijunjian886 <jijunjian886@163.com>
2019-05-23 17:08:24 +0800

    eureka-producer-consumer demo modify
    Signed-off-by: jijunjian886 <jijunjian886@163.com>

--- eureka-producer-consumer/spring-cloud-consumer/src/main/java/com/neo/remote/HelloRemote.java
+++ eureka-producer-consumer/spring-cloud-consumer/src/main/java/com/neo/remote/HelloRemote.java
@@ -1,8 +1,7 @@
 package com.neo.remote;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name= "spring-cloud-producer")
 public interface HelloRemote {
 
-    @RequestMapping(value = "/hello")
-    public String hello(@RequestParam(value = "name") String name);
-
-
+    @GetMapping(value = "/hello/say")
+    String say(@RequestParam(value = "name") String name);
 }
--- eureka-producer-consumer/spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
+++ eureka-producer-consumer/spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
@@ -1,14 +1,15 @@
 package com.neo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-	
-    @RequestMapping("/hello")
-    public String index(@RequestParam String name) {
-        return "hello "+name+"，this is first messge";
+    @GetMapping("/say")
+    public String say(String name) {
+        System.err.println("I say...");
+        return "Hello " + name + "，this is first message";
     }
 }
